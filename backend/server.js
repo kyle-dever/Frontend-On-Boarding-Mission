15cce849d1bcc6fe875da98aa899bc123816e94b
kyle-dever <kyle.park@devercorp.com>
2023-07-07 09:48:34 +0900

    feat: MySQL과 연동하여 데이터 불러오기

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import connection from "./database.js";
 const app = express();
 const port = 3000;
+
+app.get("/api/", (req, res) => {
+    connection.query("SELECT * FROM User", (err, data) => {
+        res.send(data);
+      });
+});
+
 
 app.get('/', (req, res) => {
     res.json({
