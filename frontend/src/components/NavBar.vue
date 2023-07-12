3a9f74683ae867620c4aefc044999d335374fe1b
kyle-dever <kyle.park@devercorp.com>
2023-07-12 15:53:38 +0900

    fix : routing 방식 수정

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <b-navbar toggleable="lg" type="dark" variant="info" fixed="top" sticky="true">
-            <b-navbar-brand href="/">DEVER's Developer Blog</b-navbar-brand>
+            <b-navbar-brand @click="moveToHome">DEVER's Developer Blog</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="/board/machine">머신러닝</b-nav-item>
-                    <b-nav-item href="/board/data">데이터분석</b-nav-item>
-                    <b-nav-item href="/board/program">프로그래밍</b-nav-item>
+                    <b-nav-item @click="moveToMachineBoard">머신러닝</b-nav-item>
+                    <b-nav-item @click="moveToDataBoard">데이터분석</b-nav-item>
+                    <b-nav-item @click="moveToProgrammingBoard">프로그래밍</b-nav-item>
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item href="#" @click="handleLogin">{{ loginText }}</b-nav-item>
-                    <b-nav-item href="#" @click="signIn" v-if="!loginStore.isLogin">회원가입</b-nav-item>
+                    <b-nav-item @click="handleLogin">{{ loginText }}</b-nav-item>
+                    <b-nav-item @click="signIn" v-if="!loginStore.isLogin">회원가입</b-nav-item>
                     
                 </b-navbar-nav>
             </b-collapse>
@@ -36,15 +36,30 @@ import SignInModal from './modal/SignInModal.vue';
 
 import { ref } from 'vue';
 
+import { useRouter } from 'vue-router';
 import { useModalStore } from '@/stores/modal';
 import { useListStore } from '@/stores/token';
 import { useLoginStore } from '@/stores/isLogin';
 
 import { postLogin, postSignIn } from '@/api/userApi.js';
 
+const router = useRouter();
 const modalStore = useModalStore();
 const listStore = useListStore();
 const loginStore = useLoginStore();
+
+function moveToHome() {
+  router.push('/');
+}
+function moveToMachineBoard() {
+  router.push('/board/machine');
+}
+function moveToDataBoard() {
+  router.push('/board/data');
+}
+function moveToProgrammingBoard() {
+  router.push('/board/program');
+}
 
 function getUserInfo(email, password) {
   const accessToken = postLogin(email, password)
