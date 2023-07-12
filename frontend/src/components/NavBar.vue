0ad8c9c0fbb38240dfc48014930622348c8f37e0
kyle-dever <kyle.park@devercorp.com>
2023-07-12 13:36:25 +0900

    fix: brand 이름 수정

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <b-navbar toggleable="lg" type="dark" variant="info" fixed="top" sticky="true">
-            <b-navbar-brand href="/">기술 블로그</b-navbar-brand>
+            <b-navbar-brand href="/">DEVER's Developer Blog</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
@@ -14,7 +14,7 @@
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
                     <b-nav-item href="#" @click="handleLogin">{{ loginText }}</b-nav-item>
-                    <b-nav-item href="#" @click="signIn" v-if="loginStore.isLogin">회원가입</b-nav-item>
+                    <b-nav-item href="#" @click="signIn" v-if="!loginStore.isLogin">회원가입</b-nav-item>
                     
                 </b-navbar-nav>
             </b-collapse>
