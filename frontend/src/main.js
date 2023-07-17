b0292e7164f17c875ea2785114786d8830dabe76
kyle-dever <kyle.park@devercorp.com>
2023-07-17 09:55:13 +0900

    feat: 게시글 작성 페이지 구현

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,22 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+const app = createApp(App);
+
 import { createPinia } from 'pinia';
+const pinia = createPinia();
+app.use(pinia);
 
 import router from './router/router';
-import App from './App.vue';
+app.use(router);
 
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
+loadFonts();
+app.use(vuetify);
 
 import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-
-loadFonts();
-
-const app = createApp(App);
-const pinia = createPinia();
-app.use(pinia);
-app.use(router);
 app.use(BootstrapVue3);
-app.use(vuetify);
+
 app.mount('#app');
--- frontend/src/pages/BoardWrite.vue
+++ frontend/src/pages/BoardWrite.vue
@@ -10,15 +10,18 @@
       <label for="category">카테고리 : </label>
       <select id="category" v-model="category" required>
         <option value="">카테고리 선택</option>
-        <option value="카테고리1">머신러닝</option>
-        <option value="카테고리2">데이터 분석</option>
-        <option value="카테고리3">인공지능</option>
+        <option value="머신러닝">머신러닝</option>
+        <option value="데이터 분석">데이터 분석</option>
+        <option value="인공지능">인공지능</option>
       </select>
     </div>
 
     <div class="form-item">
       <label for="content">내용</label>
-      <editor-content id="content" :editor="editor" />
+      <quill-editor
+        id="content"
+        @change="onEditorChange($event)"
+      ></quill-editor>
     </div>
 
     <div class="form-item">
@@ -26,6 +29,27 @@
     </div>
   </form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { quillEditor } from 'vue3-quill';
+
+const title = ref('');
+const category = ref('');
+const content = ref('');
+
+const onEditorChange = (e) => {
+  content.value = e.html;
+};
+
+const submitForm = () => {
+  // 게시글 작성 폼 제출 시 실행되는 로직
+  console.log('제목:', title.value);
+  console.log('카테고리:', category.value);
+  // 여기서 게시글 작성 로직을 처리하면 됩니다.
+  console.log('내용:', content.value);
+};
+</script>
 
 <style>
 .header {
@@ -46,37 +70,10 @@
 }
 
 input,
-select,
-#content {
+select {
   margin-left: 10px;
   border: 1px gray solid;
   border-radius: 5px;
   padding: 5px;
 }
 </style>
-
-<script setup>
-import { ref } from 'vue';
-import { useEditor, EditorContent } from '@tiptap/vue-3';
-import StarterKit from '@tiptap/starter-kit';
-
-const editor = useEditor({
-  content: '<p></p>',
-  extensions: [StarterKit],
-  onUpdate: ({ editor }) => {
-    content.value = editor.getHTML();
-  },
-});
-
-const title = ref('');
-const category = ref('');
-const content = ref('');
-
-const submitForm = () => {
-  // 게시글 작성 폼 제출 시 실행되는 로직
-  console.log('제목:', title.value);
-  console.log('카테고리:', category.value);
-  console.log('내용:', content.value);
-  // 여기서 게시글 작성 로직을 처리하면 됩니다.
-};
-</script>
