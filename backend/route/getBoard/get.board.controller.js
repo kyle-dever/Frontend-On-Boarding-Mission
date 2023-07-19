e5fcbd56de836771aaf786cdd05b4b8f9440e35c
kyle-dever <kyle.park@devercorp.com>
2023-07-19 16:34:05 +0900

    feat: total count 인자 이동 및 next field 존재 여부 확인

--- backend/route/getBoard/get.board.controller.js
+++ backend/route/getBoard/get.board.controller.js
@@ -14,33 +14,39 @@ export const getList = (req, res) => {
 
 export const getCount = (req, res) => {
   const database = new Database();
-
-  const category = req.query.category;
-  const boardQuery = `SELECT COUNT(*) AS count FROM Board WHERE category = ?`;
-
-  database.query(boardQuery, category).then((result) => {
-    return res.send({
-      status: 200,
-      count: result[0].count,
-    });
-  });
 };
 
 export const getListFromCategory = (req, res) => {
   const database = new Database();
 
   const category = req.query.category;
-  let page;
-  if (req.query.page) page = req.query.page;
-  else page = 1;
+  const countQuery = `SELECT COUNT(*) AS count FROM Board WHERE category = ?`;
 
-  const boardQuery = `SELECT * FROM Board WHERE category = ? ORDER BY board_id DESC LIMIT 10 OFFSET ${
-    (page - 1) * 10
-  }`;
-  database.query(boardQuery, [category, page]).then((result) => {
-    return res.send({
-      status: 200,
-      boards: result,
+  let count;
+  database
+    .query(countQuery, category)
+    .then((result) => {
+      count = result[0].count;
+
+      let page;
+      if (req.query.page && req.query.page > 1) page = req.query.page;
+      else page = 1;
+
+      let offset;
+      if (page == 1) offset = 0;
+      else offset = (page - 1) * 10;
+
+      const boardQuery = `SELECT * FROM Board WHERE category = ? ORDER BY board_id DESC LIMIT 10 OFFSET ${offset}`;
+
+      return database.query(boardQuery, [category, page]);
+    })
+    .then((result) => {
+      const hasNext = result.length == 10 ? true : false;
+      return res.send({
+        status: 200,
+        boards: result,
+        totalCount: count,
+        hasNext: hasNext,
+      });
     });
-  });
 };
