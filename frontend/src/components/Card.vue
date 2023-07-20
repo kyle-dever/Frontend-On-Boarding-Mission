8a22862410384a42cc34699d5a4ba0ce61c1ccb0
kyle-dever <kyle.park@devercorp.com>
2023-07-20 14:23:01 +0900

    feat: Home Card View 이식

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-row class="align-self-stretch">
+      <b-card
+        v-for="item in props.datas"
+        :key="item.board_id"
+        :title="item.title"
+        :img-src="item.thumbnail"
+        img-alt="No Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem; height: 20rem; margin: 0 auto"
+        class="mb-2"
+        :footer="timestamp(item.created_at)"
+        footer-tag="footer"
+        @click="emits('getBoard', item.board_id)"
+      >
+        <b-card-text> {{ item.writer }} </b-card-text>
+      </b-card>
+    </b-row>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import moment from 'moment-timezone';
+import 'moment/locale/ko';
+
+moment.tz.setDefault('Asia/Seoul');
+
+const props = defineProps(['datas']);
+const emits = defineEmits(['getBoard']);
+
+const timestamp = (createdAt) => {
+  return moment(createdAt).fromNow();
+};
+</script>
+
+<style></style>
