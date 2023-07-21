570e1e12021689ff286e7a2f20da3e011e06d3c4
kyle-dever <kyle.park@devercorp.com>
2023-07-21 09:16:58 +0900

    feat: 검색 화면 생성

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -21,18 +21,7 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form @submit.prevent="handleSearch">
-            <b-form-input
-              required
-              size="sm"
-              class="mr-sm-2"
-              placeholder="Search"
-              v-model="keyword"
-            ></b-form-input>
-            <b-button size="sm" class="my-0 my-sm-0" type="submit"
-              >Search</b-button
-            >
-          </b-nav-form>
+          <b-button type="submit" @click="moveToSearch">검색</b-button>
 
           <b-nav-item @click="handleLogin" v-if="!loginStore.isLogin"
             >로그인</b-nav-item
@@ -71,8 +60,6 @@ import { useLoginStore } from '@/stores/isLogin';
 
 import { postLogin, postSignIn } from '@/api/userApi.js';
 
-import { ref } from 'vue';
-
 const router = useRouter();
 const route = useRoute();
 const modalStore = useModalStore();
@@ -93,6 +80,9 @@ function moveToAIBoard() {
 }
 function moveToBoardWrite() {
   router.push('/board/Write');
+}
+function moveToSearch() {
+  router.push('/search');
 }
 
 function getUserInfo(email, password) {
@@ -123,12 +113,6 @@ function handleLogin() {
   } else {
     loginStore.changeStatus();
   }
-}
-
-const keyword = ref(null);
-
-function handleSearch() {
-  console.log(keyword.value);
 }
 
 function boardWrite() {
---
+++ frontend/src/pages/Search.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="submitSearch">
+    <div>
+      <label for="searchTerm">검색어:</label>
+      <input type="text" id="searchTerm" v-model="searchTerm" />
+    </div>
+    <div>
+      <label for="category">카테고리:</label>
+      <select id="category" v-model="selectedCategory">
+        <option value="all">전체</option>
+        <option value="머신러닝">머신러닝</option>
+        <option value="데이터 분석">데이터 분석</option>
+        <option value="인공지능">인공지능</option>
+      </select>
+    </div>
+    <div>
+      <label>검색 옵션:</label>
+      <label>
+        <input type="radio" value="all" v-model="searchOption" />
+        전체
+      </label>
+      <label>
+        <input type="radio" value="title" v-model="searchOption" />
+        제목
+      </label>
+      <label>
+        <input type="radio" value="content" v-model="searchOption" />
+        내용
+      </label>
+      <label>
+        <input type="radio" value="writer" v-model="searchOption" />
+        작성자
+      </label>
+    </div>
+    <b-button type="submit" @click="handleSearch">검색</b-button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const searchTerm = ref('');
+const selectedCategory = ref('all');
+const searchOption = ref('all');
+
+const handleSearch = () => {
+  console.log(searchTerm.value, selectedCategory.value, searchOption.value);
+};
+</script>
+
+<style scoped>
+label {
+  margin: 10px;
+}
+
+input {
+  border: 1px gray solid;
+  border-radius: 5px;
+  padding: 5px;
+}
+</style>
